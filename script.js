2b6982ed1790986032f1d5a8458a5f0fbb618fae
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-27 21:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-document.getElementById("btn").addEventListener("click",alertDisplay);
-
 function alertDisplay(event) {
-	  event.preventDefault();
-	let age = document.getElementById("age").value;
-	let name = document.getElementById("name").value;
-	if(age === "" || name === ""){
-		alert("Input cannot be empty");
-		return;
-	}
-	return new Promise((resolve,reject)=>{
-		if(age >= 18){
-			resolve()
-		}else{
-			reject()
-		}
-	}).then(()=>{
-		setTimeout(()=>{
-			alert(`Welcome, ${name}. You can vote.`);
-		},4000)
-	}).catch(()=>{
-		alert(`Oh sorry ${name}. You aren't old enough.`);
-	})
+    event.preventDefault();
+    let age = parseInt(document.getElementById("age").value);
+    let name = document.getElementById("name").value;
+    if(isNaN(age) || name === ""){
+        alert("Input cannot be empty or invalid");
+        return;
+    }
+    return new Promise((resolve,reject)=>{
+        if(age >= 18){
+            resolve()
+        }else{
+            reject()
+        }
+    }).then(()=>{
+        setTimeout(()=>{
+            alert(`Welcome, ${name}. You can vote.`);
+        },4000)
+    }).catch(()=>{
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+    })
 }
