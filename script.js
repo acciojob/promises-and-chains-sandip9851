5faf0d0df147fd4e4fe1c95f710b2ffe17312bf1
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-27 21:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click",alertDisplay);
+
+function alertDisplay() {
+	let age = document.getElementById("age").value;
+	let name = document.getElementById("name").value;
+	if(age === "" || name === ""){
+		alert("Input cannot be empty");
+		return;
+	}
+	return new Promise((resolve,reject)=>{
+		if(age >= 18){
+			resolve()
+		}else{
+			reject()
+		}
+	}).then(()=>{
+		setTimeout(()=>{
+			alert(`Welcome, ${name}. You can vote.`);
+		},4000)
+	}).catch(()=>{
+		alert(`Oh sorry ${name}. You aren't old enough.`);
+	})
+}
